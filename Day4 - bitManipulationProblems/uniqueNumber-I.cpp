0e54462d6c472d30979b677fca774db5116ae8a1
MemoMeto <t.a.metwally35@gmail.com>
2024-07-10 23:15:06 +0300

    Day 4\

---
+++ Day4 - bitManipulationProblems/uniqueNumber-I.cpp
@@ -0,0 +1,15 @@
+// find the unique number in an array of integers that each integer is repeated exactly once except for one number; 
+// note that the XOR of two numbers is zero; eg, 4 ^ 4 = 0; 
+
+#include<iostream> 
+#include<array> 
+using namespace std; 
+
+int main(){
+  int arr[7] = {3, 4, 1, 7, 3, 1, 7}; 
+  int ans=0;
+  for(auto x : arr){
+    ans ^= x; // performs XOR operation 
+  }
+  cout << ans; // 4
+}
