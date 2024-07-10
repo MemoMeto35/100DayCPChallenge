3b1556393b85673c33f17fc972fcbfa408e855a9
MemoMeto <t.a.metwally35@gmail.com>
2024-07-10 21:33:52 +0300

    Day 3

---
+++ Day3-bitManipulation/binaryNot.cpp
@@ -0,0 +1,13 @@
+// one important train is that x + (~x) +1 = 0, so (~x) = -x - 1;
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+	int x = 0;
+	cout << (~x) <<endl; // -1
+
+
+	return 0;
+}
