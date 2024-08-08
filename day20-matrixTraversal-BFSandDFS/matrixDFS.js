9b1d24039af0f2721e41c05df2b0a243a1e314d9
MemoMeto <t.a.metwally35@gmail.com>
2024-08-08 06:16:50 +0300

    Create matrixDFS.js

---
+++ day20-matrixTraversal-BFSandDFS/matrixDFS.js
@@ -0,0 +1,39 @@
+// Depth First Search
+// time: O(n) because we touch every node once; 
+// space: O(n) due to storing all elements in a seen array and the recursive stack call is n at maximum;
+const testMatrix = [
+  [1, 2, 3, 4, 5],
+  [6, 7, 8, 9, 10],
+  [11, 12, 13, 14, 15],
+  [16, 17, 18, 19, 20]
+];
+const directions = [
+  [-1, 0], //up
+  [0, 1], //right
+  [1, 0], //down
+  [0, -1] //left
+]
+const traversalDFS = function(matrix) {
+  const seen = 
+    new Array(matrix.length).fill(0).map(() => new Array(matrix[0].length).fill(false)); // creating a boolean array of the visited nodes; 
+
+  const values = [];
+
+  dfs(matrix, 0, 0, seen, values);
+
+  return values;
+}
+
+const dfs = function(matrix, row, col, seen, values) {
+  if(row < 0 || col < 0 || row >= matrix.length || col >= matrix[0].length || seen[row][col]) return;
+  
+  seen[row][col] = true;
+  values.push(matrix[row][col]);
+
+  for(let i = 0; i < directions.length; i++) {
+    const currentDir = directions[i];
+    dfs(matrix, row + currentDir[0], col + currentDir[1], seen, values);
+  }
+}
+
+console.log(traversalDFS(testMatrix));
