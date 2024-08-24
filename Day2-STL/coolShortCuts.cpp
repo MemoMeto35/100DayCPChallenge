0617a63c1ce1b21e7b0e458b13bc8b702fe2f92b
MemoMeto <t.a.metwally35@gmail.com>
2024-08-24 16:36:55 +0300

    Update coolShortCuts.cpp

--- Day2-STL/coolShortCuts.cpp
+++ Day2-STL/coolShortCuts.cpp
@@ -1,6 +1,7 @@
 int main(){
 int minSum = *min_element(nums.begin(), nums.end()); // gets the smallest elemetn *for the pointer
 int maxSum = accumulate(nums.begin(), nums.end(), 0/*initial sum*/); // sums all elements; 
+partial_sum(arr.begin(),arr.end(),pref.begin()) // creates a prefix sum array in pref[];
 int maxElement = *max_element(nums.begin(), nums.end()); // gets the max element;
 int mid = left + (right - left) / 2; // better than (right+left)/2 because it doesn't overflow;
 int res = (result + mod) % mod; // to get rid of negative mods;
