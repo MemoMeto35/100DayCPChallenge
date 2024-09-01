1d427950e980ce0b1b73fd32a1cc2df628034491
MemoMeto <t.a.metwally35@gmail.com>
2024-09-01 18:19:44 +0300

    reviewAgain

---
+++ day27-directedGraphsAndSCC/topological-Order.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std; 
+const int N = 1e5 + 1; 
+vector<int> gr[N]; 
+vector<int> order; 
+int vis[N]; 
+void dfs(int cur, int par){
+  vis[cur] = 1; 
+  for(auto x : gr[cur]){
+    if(!vis[x]){
+      dfs(x, cur);
+    }
+  }
+  // back from subtree; 
+  order.push_back(cur);
+  return; 
+}
+int main(){
+  int n, m; 
+  cin >> n >> m; 
+  for(itn i = 0; i<m; i++){
+    int x, y; 
+    cin >> x >> y; 
+    gr[x].push_back(y);
+  }
+  for(int i = 1; i<=n; i++){
+    if(!vis[i]) dfs(i, 0);
+  }
+  reverse(order.begin(), order.end()); 
+  for(auto x : order) cout << x << " ";
+  return 0; 
+}
