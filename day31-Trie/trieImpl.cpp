a91e673b3b838c06d986b6026af7d9be6c2d7df1
MemoMeto <t.a.metwally35@gmail.com>
2024-09-02 21:45:54 +0300

    Create trieImpl.cpp

---
+++ day31-Trie/trieImpl.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+using namespace std; 
+struct ndoe{
+  node *nxt[26]; 
+  bool is_end; 
+  node(){
+    for(int i = 0; i<26; i++){
+      nxt[i] = NULL; 
+      is_end = false; 
+    }
+  }
+};
+node *root; 
+void insert_trie(string s){
+  node *cur = root; 
+  for(int i =0; i<s.size(); i++){
+    int imap = s[i] - 'a';
+    // new node; 
+    if(cur->nxt[imap] == NULL){
+      cur->nxt[imap] = new node(); 
+    }
+    // go to that nodes; 
+    cur = cur->nxt[imap]; 
+  }
+  // cur -> last node; 
+  cur->is_end = true; 
+}
+
+// O(s.size()); 
+bool search_trie(string s){
+  node *cur = root; 
+  for(int i = 0; i<s.size(); i++){
+    int imap = s[i] - 'a'; 
+    if(cur->nxt[imap] == NULL) return false; 
+    cur = cur->nxt[imap]; 
+  }
+  return cur->is_end; 
+}
+
+int main(){
+  root = new node(); 
+  int n ; 
+  cin >> n; 
+  for(int i =0; i<n; i++){
+    string s; 
+    cin >> s; 
+    insert_trie(s); 
+  }
+  if(search_trie("ahm")){
+    cout << "Found";
+  }
+  else {
+    cout << "Not Found";
+  }
+  return 0;
+}
