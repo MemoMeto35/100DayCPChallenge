93736eb3b493e2092a89ed48df8aa541045e1825
MemoMeto <t.a.metwally35@gmail.com>
2024-09-14 05:53:10 +0300

    Create 1-diceCombinations.cpp

---
+++ day34-CSES-dynamicProgramming/1-diceCombinations.cpp
@@ -0,0 +1,89 @@
+/*
+    Always pray and trust Allah:) ||
+    keep up the good work, and never look back! ||
+    Do something instead of NOTHING ||
+    Always Stay Oranized||
+    Memoize Solutions to solve harder Ones;
+*/
+
+#include<bits/stdc++.h>
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+
+// using namespace __gnu_pbds;
+
+using namespace std;
+
+#define int            long long int
+#define F              first
+#define S              second
+#define pb             push_back
+#define si             set <int>
+#define vi             vector <int>
+#define pii            pair <int, int>
+#define vpi            vector <pii>
+#define vpp            vector <pair<int, pii>>
+#define mii            map <int, int>
+#define mpi            map <pii, int>
+#define spi            set <pii>
+#define mat            vector<vector<int>> 
+#define endl           "\n"
+#define sz(x)          ((int) x.size())
+#define all(p)         p.begin(), p.end()
+#define rall(p)           p.rbegin(), p.rend()
+#define double         long double
+#define que_max        priority_queue <int>
+#define que_min        priority_queue <int, vi, greater<int>>
+#define bug(...)       __f (#__VA_ARGS__, __VA_ARGS__)
+#define print(a)       for(auto x : a) cout << x << " "; cout << endl
+#define print1(a)      for(auto x : a) cout << x.F << " " << x.S << endl
+#define print2(a,x,y)  for(int i = x; i < y; i++) cout<< a[i]<< " "; cout << endl
+
+
+
+// template<class T> using ordered_set =tree<T, null_type, less<T>, rb_tree_tag,tree_order_statistics_node_update> ;
+// s.find_by_order(); s.order_by_key(); those two return iterators; 
+
+template <typename Arg1>
+void __f (const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << endl; }
+template <typename Arg1, typename... Args>
+void __f (const char* names, Arg1&& arg1, Args&&... args)
+{
+    const char* comma = strchr (names + 1, ',');
+    cout.write (names, comma - names) << " : " << arg1 << " | "; __f (comma + 1, args...);
+}
+
+    int const mod = 1e9+7;
+void solve() {
+
+        int n, m; 
+        cin >> n;
+        
+        vi dp(n+1);
+        dp[0] = 1;
+        for (int i = 1; i <= n; ++i)
+        {
+            int x= 6;
+            for (int j = 1; j <= min(x, i); ++j)
+            {
+                dp[i] = (dp[i]+dp[i-j])%mod;
+            }
+        }
+
+        cout << dp[n];
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+
+    clock_t z = clock();
+
+    int t = 1;
+    // cin >> t;
+    while (t--) solve();
+
+    cerr << "Run Time : " << ((double)(clock() - z) / CLOCKS_PER_SEC) << endl;
+
+    return 0;
+}
