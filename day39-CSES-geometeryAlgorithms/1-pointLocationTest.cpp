881cb8fe99d55f0e0afd33a00f228a9575a4aa0b
MemoMeto <t.a.metwally35@gmail.com>
2024-09-14 17:09:23 +0300

    cross product

---
+++ day39-CSES-geometeryAlgorithms/1-pointLocationTest.cpp
@@ -0,0 +1,90 @@
+/*
+    Always pray and trust Allah:) ||
+    keep up the good work, and never look back! ||
+    Do something instead of NOTHING ||
+    Always Stay Oranized||
+    Memoize Solutions to solve harder Ones;
+*/
+
+#include<bits/stdc++.h>
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+
+// using namespace __gnu_pbds;
+
+using namespace std;
+
+#define int            long long int
+#define F              first
+#define S              second
+#define pb             push_back
+#define si             set <int>
+#define vi             vector <int>
+#define pii            pair <int, int>
+#define vpi            vector <pii>
+#define vpp            vector <pair<int, pii>>
+#define mii            map <int, int>
+#define mpi            map <pii, int>
+#define spi            set <pii>
+#define mat            vector<vector<int>> 
+#define endl           "\n"
+#define sz(x)          ((int) x.size())
+#define all(p)         p.begin(), p.end()
+#define rall(p)           p.rbegin(), p.rend()
+#define double         long double
+#define que_max        priority_queue <int>
+#define que_min        priority_queue <int, vi, greater<int>>
+#define bug(...)       __f (#__VA_ARGS__, __VA_ARGS__)
+#define print(a)       for(auto x : a) cout << x << " "; cout << endl
+#define print1(a)      for(auto x : a) cout << x.F << " " << x.S << endl
+#define print2(a,x,y)  for(int i = x; i < y; i++) cout<< a[i]<< " "; cout << endl
+
+
+
+// template<class T> using ordered_set =tree<T, null_type, less<T>, rb_tree_tag,tree_order_statistics_node_update> ;
+// s.find_by_order(); s.order_by_key(); those two return iterators; 
+
+template <typename Arg1>
+void __f (const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << endl; }
+template <typename Arg1, typename... Args>
+void __f (const char* names, Arg1&& arg1, Args&&... args)
+{
+    const char* comma = strchr (names + 1, ',');
+    cout.write (names, comma - names) << " : " << arg1 << " | "; __f (comma + 1, args...);
+}
+
+
+
+void solve() {
+
+    int x1, y1, x2, y2, x, y;
+    cin >> x1 >> y1 >> x2 >> y2 >> x >> y;
+
+    // Calculate the cross product
+    int cross_product = (x2 - x1) * (y - y1) - (y2 - y1) * (x - x1);
+
+    if (cross_product > 0) {
+        cout << "LEFT" << endl;
+    } else if (cross_product < 0) {
+        cout << "RIGHT" << endl;
+    } else {
+        cout << "TOUCH" << endl;
+    }
+        
+
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+
+    clock_t z = clock();
+
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+
+    cerr << "Run Time : " << ((double)(clock() - z) / CLOCKS_PER_SEC) << endl;
+
+    return 0;
+}
