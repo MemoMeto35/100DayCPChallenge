643194f6ba46930aedebef80b6da7ac3b59a13e9
MemoMeto <t.a.metwally35@gmail.com>
2024-11-04 01:36:48 +0200

    Create Sakurako and Kosuke.cpp

---
+++ day42-10(Div3)Contests/CF 981/Sakurako and Kosuke.cpp
@@ -0,0 +1,43 @@
+/*
+    Always Stay Organized ||
+    Memoize Solutions to solve harder Ones;
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+#define int            long long int
+#define ar             array
+#define endl           "\n"
+#define all(p)         p.begin(), p.end()
+#define bug(...)       __f (#__VA_ARGS__, __VA_ARGS__)
+
+template <typename Arg1>
+void __f (const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << endl; }
+template <typename Arg1, typename... Args>
+void __f (const char* names, Arg1&& arg1, Args&&... args)
+{
+    const char* comma = strchr (names + 1, ',');
+    cout.write (names, comma - names) << " : " << arg1 << " | "; __f (comma + 1, args...);
+}
+
+int n, m;
+const int maxN = 2e5;
+
+void solve() {
+    cin >> n;
+    if(n&1)
+        cout << "Kosuke" << endl;
+    else 
+        cout << "Sakurako" << endl;
+
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+    return 0;
+}
